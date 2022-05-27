65112019d818d1741c2dbfd26e44cc935d4df21d
FlorianMs59 <67105434+FlorianMs59@users.noreply.github.com>
2022-05-27 16:20:57 +0200

    scroll header

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeaer() {
+    const header = document.getElementById('header');
+
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+
+/*=============== SWIPER POPULAR ===============*/
+
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
