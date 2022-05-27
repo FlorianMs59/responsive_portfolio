ff21062182c7419d76431aa77fe58cee86f8dcc9
FlorianMs59 <67105434+FlorianMs59@users.noreply.github.com>
2022-05-27 16:33:52 +0200

    scroll header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeaer() {
+function scrollHeader() {
     const header = document.getElementById('header');
 
     if(this.scrollY >= 50) header.classList.add('scroll-header');
